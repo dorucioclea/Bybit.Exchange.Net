21d4b689a6c765de82f7b4af1d9957d43c1346d9
khang152 <121246182+Khang152@users.noreply.github.com>
2024-04-06 23:56:26 +0700

    [Institutional Loan] Add Get Margin Coin Info

---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/API/V5/InstitutionalLoan/GetMarginCoinInfo.cs
@@ -0,0 +1,25 @@
+﻿using Bybit.Exchange.Net.Library;
+using Bybit.Exchange.Net.Models.Common;
+using Bybit.Exchange.Net.Models.V5.InstitutionalLoan;
+
+namespace Bybit.Exchange.Net.API
+{
+    public partial class V5
+    {
+        public partial class Endpoint
+        {
+            public partial class InstitutionalLoan
+            {
+                private string GetMarginCoinInfoUrl { get; set; } = "/v5/ins-loan/ensure-tokens-convert";
+
+                public async Task<BybitResponse<GetMarginCoinInfoResponse>> GetMarginCoinInfoAsync(GetMarginCoinInfoRequest requestData)
+                {
+                    var requestUrl = Utils.GetUrl(Options, GetMarginCoinInfoUrl);
+                    var response = await Utils.GetData(Options, requestUrl, requestData, useAPIKey: true);
+                    var results = Utils.GetResponse<GetMarginCoinInfoResponse>(response);
+                    return results;
+                }
+            }
+        }
+    }
+}
---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/Models/V5/InstitutionalLoan/GetMarginCoinInfo.cs
@@ -0,0 +1,82 @@
+﻿namespace Bybit.Exchange.Net.Models.V5.InstitutionalLoan
+{
+    public class GetMarginCoinInfoRequest
+    {
+        /// <summary>
+        /// <value>Property <c>productId</c></value>
+        /// <para>
+        /// Product Id. If not passed, then return all products info
+        /// </para>
+        /// </summary>
+        public string? productId { get; set; } = default!;
+    }
+
+
+    public class GetMarginCoinInfoResponse
+    {
+        /// <summary>
+        /// <value>Property <c>marginToken</c></value>
+        /// <para>
+        /// Array of margin tokens.
+        /// </para>
+        /// </summary>
+        public MarginTokenItem[] MarginToken { get; set; } = default!;
+
+        public class MarginTokenItem
+        {
+            /// <summary>
+            /// <value>Property <c>productId</c></value>
+            /// <para>
+            /// Product Id
+            /// </para>
+            /// </summary>
+            public string ProductId { get; set; } = default!;
+
+            /// <summary>
+            /// <value>Property <c>tokenInfo</c></value>
+            /// <para>
+            /// Array of Spot margin coins.
+            /// </para>
+            /// </summary>
+            public TokenInfoItem[] TokenInfo { get; set; } = default!;
+        }
+
+        public class TokenInfoItem
+        {
+            /// <summary>
+            /// <value>Property <c>token</c></value>
+            /// <para>
+            /// Margin coin
+            /// </para>
+            /// </summary>
+            public string Token { get; set; } = default!;
+
+            /// <summary>
+            /// <value>Property <c>convertRatioList</c></value>
+            /// <para>
+            /// Array of Margin coin convert ratio List.
+            /// </para>
+            /// </summary>
+            public ConvertRatioListItem[] ConvertRatioList { get; set; } = default!;
+        }
+
+        public class ConvertRatioListItem
+        {
+            /// <summary>
+            /// <value>Property <c>ladder</c></value>
+            /// <para>
+            /// ladder
+            /// </para>
+            /// </summary>
+            public string ladder { get; set; } = default!;
+
+            /// <summary>
+            /// <value>Property <c>convertRatio</c></value>
+            /// <para>
+            /// Margin coin convert ratio
+            /// </para>
+            /// </summary>
+            public string convertRatio { get; set; } = default!;
+        }
+    }
+}
