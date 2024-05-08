8938d35239893de937aeebd314e64d7be54be759
khang152 <121246182+Khang152@users.noreply.github.com>
2024-05-08 16:45:36 +0700

    [Account] Add Demo Apply Money API

---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/API/V5/Account/DemoApplyMoney.cs
@@ -0,0 +1,25 @@
+﻿using Bybit.Exchange.Net.Library;
+using Bybit.Exchange.Net.Models.Common;
+using Bybit.Exchange.Net.Models.V5.Account;
+
+namespace Bybit.Exchange.Net.API
+{
+    public partial class V5
+    {
+        public partial class Endpoint
+        {
+            public partial class Account
+            {
+                private string DemoApplyMoneyUrl { get; set; } = "/v5/account/demo-apply-money";
+
+                public async Task<BybitResponse<DemoApplyMoneyResponse>> DemoApplyMoneyAsync(DemoApplyMoneyRequest requestData)
+                {
+                    var requestUrl = Utils.GetUrl(Options, DemoApplyMoneyUrl);
+                    var response = await Utils.PostData(Options, requestUrl, requestData);
+                    var results = Utils.GetResponse<DemoApplyMoneyResponse>(response);
+                    return results;
+                }
+            }
+        }
+    }
+}
---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/Models/V5/Account/DemoApplyMoney.cs
@@ -0,0 +1,64 @@
+﻿using static Bybit.Exchange.Net.Data.Enums;
+
+namespace Bybit.Exchange.Net.Models.V5.Account
+{
+    public class DemoApplyMoneyRequest
+    {
+        /// <summary>
+        /// <value>Property <c>utaDemoApplyMoney</c></value>
+        /// <para>
+        /// Applied coin, supports BTC, ETH, USDT, USDC
+        /// </para>
+        /// </summary>
+        public UtaDemoApplyMoneyItem[] utaDemoApplyMoney { get; set; } = default!;
+    }
+
+    public class UtaDemoApplyMoneyItem
+    {
+        /// <summary>
+        /// <value>Property <c>coin</c></value>
+        /// <para>
+        /// Applied coin
+        /// </para>
+        /// </summary>
+        public string coin { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>amountStr</c></value>
+        /// <remarks>
+        /// Applied amount, the max applied amount in each request
+        /// </remarks>
+        /// <list type="table">
+        /// <listheader>
+        /// <term>Coin</term>
+        /// <description>Max Applied Amount</description>
+        /// </listheader>
+        /// </list>
+        /// <list type="bullet">
+        /// <item>
+        /// <term>BTC</term>
+        /// <description>"15"</description>
+        /// </item>
+        /// <item>
+        /// <term>ETH</term>
+        /// <description>"200"</description>
+        /// </item>
+        /// <item>
+        /// <term>USDT</term>
+        /// <description>"100000"</description>
+        /// </item>
+        /// <item>
+        /// <term>USDC</term>
+        /// <description>"100000"</description>
+        /// </item>
+        /// </list>
+        /// </summary>
+        public string amountStr { get; set; } = default!;
+    }
+
+
+    public class DemoApplyMoneyResponse
+    {
+
+    }
+}
