cb5eed41312e188e15ac80de3af54d0d6cc50d3a
khang152 <121246182+Khang152@users.noreply.github.com>
2024-05-08 16:47:25 +0700

    [Position] Add Set Leverage API

---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/Models/V5/Position/SetLeverage.cs
@@ -0,0 +1,51 @@
+﻿using static Bybit.Exchange.Net.Data.Enums;
+
+namespace Bybit.Exchange.Net.Models.V5.Position
+{
+    public class SetLeverageRequest
+    {
+        /// <summary>
+        /// <value>Property <c>category</c></value>
+        /// <remarks>
+        /// Product type
+        /// </remarks>
+        /// </summary>
+        public Category? category { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>symbol</c></value>
+        /// <para>
+        /// Symbol name
+        /// </para>
+        /// </summary>
+        public string symbol { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>buyLeverage</c></value>
+        /// <para>
+        /// [1, max leverage of corresponding risk limit]
+        /// </para>
+        /// <para>
+        /// One-way mode: buyLeverage must be the same as sellLeverage
+        /// </para>
+        /// <para>
+        /// Hedge mode:
+        /// Classic account &amp; UTA (isolated margin): buyLeverage and sellLeverage can be different;
+        /// UTA (cross margin): buyLeverage must be the same as sellLeverage
+        /// </para>
+        /// </summary>
+        public string buyLeverage { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>sellLeverage</c></value>
+        /// <para>
+        /// [1, max leverage of corresponding risk limit]
+        /// </para>
+        /// </summary>
+        public string sellLeverage { get; set; } = default!;
+    }
+
+    public class SetLeverageResponse
+    {
+    }
+}
