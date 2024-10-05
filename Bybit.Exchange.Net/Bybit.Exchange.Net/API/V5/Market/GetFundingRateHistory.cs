f761ef99da6103fe7c079455e00ca34c6d4fab50
khang152 <khang152.tk@gmail.com>
2024-10-05 15:17:01 +0700

    [Market] Add Get Funding Rate History API

---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/API/V5/Market/GetFundingRateHistory.cs
@@ -0,0 +1,25 @@
+﻿using Bybit.Exchange.Net.Library;
+using Bybit.Exchange.Net.Models.Common;
+using Bybit.Exchange.Net.Models.V5.Market;
+
+namespace Bybit.Exchange.Net.API
+{
+    public partial class V5
+    {
+        public partial class Endpoint
+        {
+            public partial class Market
+            {
+                private string GetFundingRateHistoryUrl { get; set; } = "/v5/market/funding/history";
+
+                public async Task<BybitResponse<GetFundingRateHistoryResponse>> GetFundingRateHistoryAsync(GetFundingRateHistoryRequest requestData)
+                {
+                    var requestUrl = Utils.GetUrl(Options, GetFundingRateHistoryUrl);
+                    var response = await Utils.GetData(Options, requestUrl, requestData, useAPIKey: false);
+                    var results = Utils.GetResponse<GetFundingRateHistoryResponse>(response);
+                    return results;
+                }
+            }
+        }
+    }
+}
---
+++ Bybit.Exchange.Net/Bybit.Exchange.Net/Models/V5/Market/GetFundingRateHistory.cs
@@ -0,0 +1,98 @@
+﻿using static Bybit.Exchange.Net.Data.Enums;
+
+namespace Bybit.Exchange.Net.Models.V5.Market
+{
+    public class GetFundingRateHistoryRequest
+    {
+        /// <summary>
+        /// <value>Property <c>category</c></value>
+        /// <remarks>
+        /// Product type:
+        /// spot, linear, inverse, option
+        /// </remarks>
+        /// </summary>
+        public required Category category { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>symbol</c></value>
+        /// <para>
+        /// Symbol name
+        /// </para>
+        /// </summary>
+        public string? symbol { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>StartTime</c></value>
+        /// <para>
+        /// Start Time
+        /// </para>
+        /// </summary>
+        public int? StartTime { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>EndTime</c></value>
+        /// <para>
+        /// End Time
+        /// </para>
+        /// </summary>
+        public int? EndTime { get; set; } = default!;
+
+
+        /// <summary>
+        /// <value>Property <c>limit</c></value>
+        /// <para>
+        /// Limit size for each bid and ask
+        /// </para>
+        /// </summary>
+        public int? limit { get; set; } = default!;
+    }
+
+    public class GetFundingRateHistoryResponse
+    {
+        /// <summary>
+        /// <value>Property <c>Category</c></value>
+        /// <para>
+        /// Product type
+        /// </para>
+        /// </summary>
+        public string? Category { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>list</c></value>
+        /// <para>
+        /// Object
+        /// </para>
+        /// </summary>
+        public List<GetFundingRateHistoryItem>? List { get; set; } = default!;
+    }
+
+    /// <summary>
+    /// ListItem class for Get Funding Rate History Item
+    /// </summary>
+    public class GetFundingRateHistoryItem
+    {
+        /// <summary>
+        /// <value>Property <c>symbol</c></value>
+        /// <para>
+        /// Symbol name
+        /// </para>
+        /// </summary>
+        public string? Symbol { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>FundingRate</c></value>
+        /// <para>
+        /// Funding rate
+        /// </para>
+        /// </summary>
+        public string? FundingRate { get; set; } = default!;
+
+        /// <summary>
+        /// <value>Property <c>FundingRateTimestamp</c></value>
+        /// <para>
+        /// Funding rate timestamp (ms)
+        /// </para>
+        /// </summary>
+        public string? FundingRateTimestamp { get; set; } = default!;
+    }
+}
